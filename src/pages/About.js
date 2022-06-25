44028d04e2731101b2aa3c7cd63c997ec6455d8c
akairosannnn <joshietrinidad@gmail.com>
2022-06-25 02:57:31 -0400

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,14 @@
-const About = () => {
+import { Link } from 'react-router-dom'
+
+function About() {
   return (
     <section className='section'>
       <h2>About</h2>
+      <Link to='/' className='btn'>
+        Back Home
+      </Link>
     </section>
   );
 };
+
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
-const Home = () => {
+import { Link } from 'react-router-dom'
+
+function Home() {
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <Link to="/about" className='btn'>About</Link>
     </section>
   );
 };
+
 export default Home;
